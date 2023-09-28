75d6111089b3e9674216bb15dd445dd809a3a3a9
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-09-28 22:59:52 +0700

    finish variable

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var name type = value
+	// var a int = 2
+	// var msg string = "Rock"
+
+	var a any = "test"
+
+	b, ok := a.(string)
+	if !ok {
+		panic("err")
+	}
+	fmt.Println(b)
 }
