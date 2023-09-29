0adb00990e31d63d1031f67d0666bc9d273bb87a
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-09-29 11:31:19 +0700

    pointer

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func swap(x, y *int) {
+	temp := *x
+	*x = *y
+	*y = temp
+}
+
+func main() {
+	// a := new(int)
+	// b := 10
+
+	// *a = b
+	// a = &b
+
+	// fmt.Printf("%p\n", a)
+	// fmt.Printf("%p\n", &b)
+	// fmt.Printf("%p\n", &a)
+	// fmt.Printf("%d\n", *a)
+
+	x := 5
+	y := 10
+
+	fmt.Println(x, y) // 5, 10
+	swap(&x, &y)
+	fmt.Println(x, y) // 10, 5
 }
