88c8d9a2de7e02f36e2e97f62643fd52de8725c0
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-09-29 09:49:39 +0700

    loop

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// initial, condition , consequences
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// while
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
 }
