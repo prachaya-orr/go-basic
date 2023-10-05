5dafe9ec11844c901db8fab1dd905dd599f32156
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-10-05 13:54:15 +0700

    struct

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/prachaya-orr/go-basic/models"
+)
+
+func Debug(obj any) {
+	raw, _ := json.MarshalIndent(&obj, "", "\t")
+	fmt.Println(string(raw))
+}
+
+type book struct {
+	*models.Book
+}
+
+func (b *book) GetBook() *book {
+	return b
+}
+
+func (b *book) SetTitle(title string) {
+	b.Title = title
+}
+
 func main() {
+	b := &book{
+		&models.Book{
+			Id:     1,
+			Title:  "Golang Basic",
+			Author: "arther",
+		},
+	}
+	b.SetTitle("ABC")
+	Debug(b.GetBook())
 
 }
