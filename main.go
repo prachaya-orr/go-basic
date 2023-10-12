dde891ad1db4ab8ac0e46fa07ab30c3b3b24cebb
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-10-13 00:00:53 +0700

    wait group / chanel

--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	// Basic -> sync.Waitgroup
+	// a := []int{1, 2, 3, 4, 5}
+
+	// var wg sync.WaitGroup
+	// for i := range a {
+	// 	wg.Add(1)
+	// 	go func(i int) {
+	// 		defer wg.Done()
+	// 		fmt.Printf("%v ", a[i])
+	// 	}(i)
+	// }
+	// wg.Wait()
+
+	// var wg sync.WaitGroup
+	// wg.Add(len(a))
+	// go func() {
+	// 	for i := range a {
+	// 		defer wg.Done()
+	// 		fmt.Printf("%v ", a[i])
+	// 	}
+	// }()
+	// wg.Wait()
+
+	// Chanel
+	numberCh := make(chan int)
+	msgCh := make(chan string)
+
+	// number
+	// chan<- int รับค่าได้อย่างเดียว ไม่ assign
+	go func(numberCh chan<- int) {
+		numberCh <- 10
+	}(numberCh)
+
+	// msg
+	go func(msgCh chan<- string) {
+		msgCh <- "hello world"
+	}(msgCh)
+
+	number := <-numberCh
+	msg := <-msgCh
+
+	fmt.Println(number)
+	fmt.Println(msg)
+
+	// Worker Pool
 
 }
